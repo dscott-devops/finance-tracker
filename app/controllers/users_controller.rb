3d1c4ad21e2604410a886ab9645e3495dd5335c2
Darryl Scott <dscott@rts-llc.com>
2021-10-03 12:34:20 +0000

    Added friendship object and add user_stocks object

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def portfolio
+    @tracked_stocks = current_user.stocks
+  end
+  def friends
+    @friends = current_user.friends
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:destroy]
   devise_for :users
   root 'welcome#index'
+  get 'portfolio', to: 'users#portfolio'
+  get 'search_stock', to: 'stocks#search'
+  get 'friends', to: 'users#friends'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
